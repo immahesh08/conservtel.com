d941623fa20d34ffa6086e6c51128e96600ce869
immahesh08 <patelmahesh1715@gmail.com>
2026-06-24 11:44:39 +0530

    Replace StrictMode with BrowserRouter
    Removed StrictMode and wrapped App component with BrowserRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode> 
+    <BrowserRouter>
     <App />
-  </StrictMode>,
+    </BrowserRouter>
+  // {/* </StrictMode>, */}
 )
