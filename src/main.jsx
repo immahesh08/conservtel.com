ead6c717472adb43a2e53c9abd6b3004098225d0
immahesh08 <patelmahesh1715@gmail.com>
2026-06-24 11:46:35 +0530

    Restore StrictMode and fix Router implementation
    Re-enable StrictMode and update Router usage.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-// import { StrictMode } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode> 
+  <StrictMode> 
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  // {/* </StrictMode>, */}
+  </StrictMode>, 
 )
